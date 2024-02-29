f388ce3811966b723711eb03c6707f73a44f376c
Rainer Stropek <rainer@software-architects.at>
2024-02-29 19:19:05 +0100

    Live demos

---
+++ live/2024-02-enums/Program.cs
@@ -0,0 +1,35 @@
+﻿const int WAGON_PASSENGER = 1;
+const int WAGON_LOCOMOTIVE = 2;
+// ...
+
+var myWagon = WAGON_LOCOMOTIVE;
+var myNewWagon = WagonType.Locomotive;
+
+System.Console.WriteLine(myWagon);
+System.Console.WriteLine(myNewWagon);
+System.Console.WriteLine((int)myNewWagon);
+
+WagonType myOtherWagon = (WagonType)5;
+System.Console.WriteLine(myOtherWagon);
+
+var fileMode = FileMode.Read | FileMode.Write;
+System.Console.WriteLine(fileMode);
+
+fileMode = (FileMode)0b1111;
+System.Console.WriteLine(fileMode);
+
+enum WagonType
+{
+    Passenger = 0,
+    Locomotive = 1,
+    CarTransport = 5
+}
+
+[Flags]
+enum FileMode
+{
+    Read = 0b0001,
+    Write = 0b0010,
+    CreateIfNotExists = 0b0100,
+    DeleteOnClose = 0b1000,
+}
---
+++ live/2024-02-hashset/Program.cs
@@ -0,0 +1,18 @@
+﻿HashSet<int> numbers = [1, 2, 3, 4, 5, 5];
+foreach (var n in numbers)
+{
+    System.Console.WriteLine(n);
+}
+
+if (numbers.Contains(3))
+{
+    System.Console.WriteLine("yes");
+}
+
+HashSet<int> otherNumbers = [1, 2, 3, 4, 5];
+if (numbers.SetEquals(otherNumbers))
+{
+    System.Console.WriteLine("Equal");
+}
+
+//System.Console.WriteLine("8".GetHashCode());
